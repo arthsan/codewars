97493779b021a012bdd78584200c1c9c92768bfa
Arthur <arthur_sbd@hotmail.com>
2019-06-24 12:28:14 -0300

    kyu8

---
+++ kyu-8/bellow-odd.js
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/count-odd-numbers-below-n/train/javascript
+
+// Given a number n, return the number of positive odd numbers below n, EASY!
+// oddCount(7) //=> 3, i.e [1, 3, 5]
+// oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+
+function oddCount(n){
+  let count = 0;
+  for(let i = 0; i < n; i += 1) {
+    if(i % 2 !== 0) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+const oddCount = (n) => {
+  if(n % 2 === 0) {
+    return (n/2)-1     
+  }
+  else {
+    return (n-1)/2;
+  }
+}
